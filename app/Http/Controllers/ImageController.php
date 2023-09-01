af07a87a636438a7caf383b946bc20f58a1137a4
David Joyce <DMJoyce951@gmail.com>
2023-09-01 11:41:53 -0400

    Changed Astrobin endpoint to have a limit of 50 to be able to get all of my images

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -4,13 +4,15 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Config;
 
 class ImageController extends Controller
 {
     public function getImages()
     {
-        $response = Http::get('http://astrobin.com/api/v1/image/?user=astro_davido&api_key=04136fb24aaeb2c8bdcb1d165aef5eaf9bd24bd6&api_secret=2a7c29d6f2cf9846df8e69ae2b65c59406535533&format=json');
-    
+        
+        $response = Http::get('http://astrobin.com/api/v1/image/?user=astro_davido&api_key=04136fb24aaeb2c8bdcb1d165aef5eaf9bd24bd6&api_secret=2a7c29d6f2cf9846df8e69ae2b65c59406535533&format=json&limit=50');
+
         $jsonData = $response->json();
           
         return $jsonData;
